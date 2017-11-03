b1d03a1b6cd675bee7341e7a1e969ed0d3d2b236
Anthony-Kim <anthonykim1985@gmail.com>
2017-11-03 17:56:19 +0900

    [ADD] parameter tableYear

--- src/main/java/org/bigdatacenter/healthcareintegrationplatform/api/RequestControllerForDataWorkFlow.java
+++ src/main/java/org/bigdatacenter/healthcareintegrationplatform/api/RequestControllerForDataWorkFlow.java
@@ -4,6 +4,7 @@ import org.bigdatacenter.healthcareintegrationplatform.domain.workflow.ScenarioT
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
@@ -18,11 +19,10 @@ public class RequestControllerForDataWorkFlow {
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
     @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "dataWorkFlow", method = RequestMethod.POST)
-    public String dataWorkFlow(@RequestParam ScenarioTask scenarioTask, HttpServletResponse httpServletResponse) {
+    @RequestMapping(value = "dataWorkFlow", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
+    // curl -v -X POST http://was.bigdatacenter.org:20780/request/workflow/api/dataWorkFlow -H "Content-Type:application/json; UTF-8" -d "{\"scenarioQueryList\":[{\"query\":\"SELECT * FROM T1\"},{\"query\":\"SELECT * FROM T2\"}]}"
+    public String dataWorkFlow(@RequestBody ScenarioTask scenarioTask, HttpServletResponse httpServletResponse) {
         logger.info(String.format("%s - ScenarioTask: %s", currentThreadName, scenarioTask));
-        httpServletResponse.setStatus(HttpServletResponse.SC_OK);
-
         final String jobAcceptedTime = dateFormat.format(new Date(System.currentTimeMillis()));
         return String.format("Accepted Time: %s", jobAcceptedTime);
     }
--- src/test/java/org/bigdatacenter/healthcareintegrationplatform/ExtractionDataRequestApiTests.java
+++ src/test/java/org/bigdatacenter/healthcareintegrationplatform/ExtractionDataRequestApiTests.java
@@ -18,23 +18,24 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//@RunWith(SpringRunner.class)
-//@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class ExtractionDataRequestApiTests {
-//    @Autowired
+    @Autowired
     private TestRestTemplate restTemplate;
 
-//    @Test
+    @Test
     public void testExtractionDataRequestApi() {
         String body = restTemplate.getForObject("/request/extraction/api/dataExtraction?dataSetUID=677", String.class);
         assertThat(body).isEqualTo("OK");
     }
 
-//    @Test
+    @Test
     public void testReadProjectionNames() {
         final MultiValueMap<String, String> parameters = new LinkedMultiValueMap<>();
         parameters.add("dataSetUID", "732");
         parameters.add("tableName", "nis_t40_2011");
+        parameters.add("tableYear", "2011");
 
         final HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -42,6 +43,6 @@ public class ExtractionDataRequestApiTests {
         HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(parameters, headers);
         String response = restTemplate.postForObject("/request/extraction/api/readProjectionNames", request, String.class);
 
-        System.out.println(response);
+        assertThat(response).isEqualTo("spec_id_sno,sick_sno,sick_dgsbjt_cd,sick_cd,dmd_dgsbjt_cd,year,data_set,jid,sex_tp_cd,agg,dif,recu_fr_dd,sido_cd,cl_cd,org_df");
     }
 }

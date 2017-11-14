7795c30fc9dd2de4743bc712c0d1df83540311f9
Anthony-Kim <anthonykim1985@gmail.com>
2017-11-14 11:21:43 +0900

    [ADD] REST call

--- src/main/java/org/bigdatacenter/healthcareintegrationplatform/api/RequestControllerForDataWorkFlow.java
+++ src/main/java/org/bigdatacenter/healthcareintegrationplatform/api/RequestControllerForDataWorkFlow.java
@@ -3,9 +3,12 @@ package org.bigdatacenter.healthcareintegrationplatform.api;
 import org.bigdatacenter.healthcareintegrationplatform.domain.workflow.ScenarioTask;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletResponse;
 import java.text.SimpleDateFormat;
@@ -18,11 +21,21 @@ public class RequestControllerForDataWorkFlow {
     private static final String currentThreadName = Thread.currentThread().getName();
     private static final SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
+    @Value("${scenario.processor.api.request.workflow}")
+    private String scenarioURL;
+
     @ResponseStatus(HttpStatus.OK)
     @RequestMapping(value = "dataWorkFlow", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE)
     // curl -v -X POST http://was.bigdatacenter.org:20780/request/workflow/api/dataWorkFlow -H "Content-Type:application/json; UTF-8" -d "{\"scenarioQueryList\":[{\"query\":\"SELECT * FROM T1\"},{\"query\":\"SELECT * FROM T2\"}]}"
     public String dataWorkFlow(@RequestBody ScenarioTask scenarioTask, HttpServletResponse httpServletResponse) {
         logger.info(String.format("%s - ScenarioTask: %s", currentThreadName, scenarioTask));
+
+        final RestTemplate restTemplate = new RestTemplate();
+        restTemplate.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+
+        final String response = restTemplate.postForObject(scenarioURL, scenarioTask, String.class);
+        logger.info(String.format("%s - response: %s", currentThreadName, response));
+
         final String jobAcceptedTime = dateFormat.format(new Date(System.currentTimeMillis()));
         return String.format("Accepted Time: %s", jobAcceptedTime);
     }

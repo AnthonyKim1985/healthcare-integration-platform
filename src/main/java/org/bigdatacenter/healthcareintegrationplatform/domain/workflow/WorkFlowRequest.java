5075112bfe4f43d40bd2e7da9f5509ea9c34326d
Anthony-Kim <anthonykim1985@gmail.com>
2017-11-22 10:58:54 +0900

    [MODIFY] dataSetUID -> TrRequestInfo

--- src/main/java/org/bigdatacenter/healthcareintegrationplatform/domain/workflow/WorkFlowRequest.java
+++ src/main/java/org/bigdatacenter/healthcareintegrationplatform/domain/workflow/WorkFlowRequest.java
@@ -3,6 +3,7 @@ package org.bigdatacenter.healthcareintegrationplatform.domain.workflow;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.bigdatacenter.healthcareintegrationplatform.domain.transaction.TrRequestInfo;
 
 import java.io.Serializable;
 
@@ -10,6 +11,6 @@ import java.io.Serializable;
 @NoArgsConstructor
 @AllArgsConstructor
 public class WorkFlowRequest implements Serializable {
-    private Integer dataSetUID;
+    private TrRequestInfo requestInfo;
     private ScenarioTask scenarioTask;
 }
